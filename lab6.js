84b36e0355748a3be7bd40c6dac60cce6d3dbabc
BavatharaniKarunatharanSecE <bavitharu13@gmail.com>
2024-10-08 09:38:22 -0400

    Add files via upload

---
+++ lab6.js
@@ -0,0 +1,26 @@
+// Task 1
+let myHumber = 10;
+let myString ="Hello, Javascript";
+console.log(myHumber);
+console.log(myString);
+
+//Task 2
+for (let i = 0; i<11; i++) {
+    console.log(i);
+}
+for (let i=1; i<6; i++) {
+    console.log(3 * i);
+}
+
+//Task 3
+function addNumbers(num1,num2) {
+    return num1 + num2;
+}
+let result = addNumbers(1, 4);
+console.log(result);
+
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+}
+let results = multiplyNumbers(8, 9);
+console.log(results);
